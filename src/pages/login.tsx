1f6c875c0bf2ad465c694fc6bfdf2066afcffd6f
Ömer Şamlı <60118914+omer-77@users.noreply.github.com>
2023-08-17 02:32:17 +0300

    fix typo

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -21,7 +21,7 @@ const LoginForm = () => {
   const initialValues: FormValues = { username: "", password: "" };
   const validationSchema = Yup.object().shape({
     username: Yup.string().min(4, "Username must be at least 4 characters long.").required("Username is required"),
-    password: Yup.string().min(5, "Password must be at least 6 characters long.").required("Password is required"),
+    password: Yup.string().min(5, "Password must be at least 5 characters long.").required("Password is required"),
   });
 
   return (
